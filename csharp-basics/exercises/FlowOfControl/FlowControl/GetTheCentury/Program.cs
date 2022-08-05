41e8e46370bab6854dea372cd517b81ef14ca06a
Markuss Sloka <markuss1777@gmail.com>
2022-08-05 12:45:28 +0300

    flow of control done

--- csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
@@ -6,7 +6,13 @@ namespace GetTheCentury
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Write year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(centuryFromYear(year) + " century");
+        }
+        public static int centuryFromYear(int year)
+        {
+            return (int)(year / 100) + ((year % 100 == 0) ? 0 : 1);
         }
     }
 }
--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,68 +4,64 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
-            var input = Console.ReadLine();
-
-            /*
-            todo - check if number
-            if (?) 
+            var input = Convert.ToInt64(Console.ReadLine());
+            
+            if (input > 0) 
             {
-                //todo - check if n is less than zero
-                if (?) 
+                
+                if (input < 0) 
                 {
-                    n *= -1;
+                    input *= -1;
                 }
+                var lenght = input.ToString().Length;
 
-                fixme
-                if (?) 
+                if (input >= 10000000000) 
                 {
                     Console.WriteLine("Number is greater or equals 10,000,000,000!");
                 } 
                 else 
                 {
                     int digits = 1;
-                    if (?) 
+                    if (lenght == 2) 
                     {
                         digits = 2;
                     } 
-                    else if (?) 
+                    else if (lenght == 3) 
                     {
                         digits = 3;
                     } 
-                    else if (?) 
+                    else if (lenght == 4) 
                     {
                         digits = 4;
                     } 
-                    else if (?) 
+                    else if (lenght == 5) 
                     {
                         digits = 5;
                     } 
-                    else if (?) 
+                    else if (lenght == 6) 
                     {
                         digits = 6;
                     } 
-                    else if (?) 
+                    else if (lenght == 7) 
                     {
                         digits = 7;
                     } 
-                    else if (?) 
+                    else if (lenght == 8) 
                     {
                         digits = 8;
                     } 
-                    else if (?) 
+                    else if (lenght == 9) 
                     {
                         digits = 9;
                     } 
-                    else if (?) 
+                    else if (lenght == 10) 
                     {
                         digits = 10;
                     }
-
                     Console.WriteLine("Number of digits in the number: " + digits);
                 }
             } 
@@ -73,7 +69,7 @@ namespace TenBillion
             {
                 Console.WriteLine("The number is not a long");
             }
-            */
+            Console.ReadKey();
         }
     }
 }
