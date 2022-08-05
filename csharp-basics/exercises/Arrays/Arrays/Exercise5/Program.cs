58ac7c8132ab093ebc726c7bdb54679d2e8b1a0a
Markuss Sloka <markuss1777@gmail.com>
2022-08-05 12:51:41 +0300

    Exercise 5 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
@@ -17,10 +17,14 @@ namespace Exercise5
             
             for (int i = 0; i < myArray.Length; i++)
             {
-                if (myArray[i] == 36 || myArray[i] == 29)
+                if (myArray[i] == 29)
+                {
+                    indexOfElement29 = i;
+                }
+
+                if (myArray[i] == 36)
                 {
                     indexOfElement36 = i;
-                    indexOfElement29 = i;
                 }
             }
             Console.WriteLine("Index position of 36 is: " + indexOfElement36);
